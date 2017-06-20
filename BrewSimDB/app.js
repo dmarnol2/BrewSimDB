f78841f575a6366013cc22bae78fc13180a2a9bb
Kristofer Hoadley <kristoferhoadley@gmail.com>
2017-06-20 16:06:24 -0500

    Now loads the database completely from sql files.

--- BrewSimDB/app.js
+++ BrewSimDB/app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
+var execSQL = require('exec-sql');
 
 var users = require('./routes/users');
 
@@ -26,17 +27,56 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/users', users);
 
 //DB setup
+
+execSQL.connect('', 'root', '');
+execSQL.executeDirectory(__dirname+'\\data', function(err) {
+    if(err) throw err;
+    execSQL.disconnect();
+    console.log('Done executing directory ' + __dirname + '\\data');
+});
+
+
 var connection = mysql.createConnection({
     host    : 'localhost',
     user    : 'root',
     database: 'BREWSIMDB'
 });
 
+/*connection.connect(function(err){
+    if(err) throw err;
+    console.log("Connected to database.");
+    connection.query("DROP DATABASE IF EXISTS brewsimdb", function(err, result){
+        if(err) throw err;
+        console.log(result);
+    });
+    connection.query("CREATE DATABASE brewsimdb", function(err, result){
+        if(err) throw err;
+        console.log(result);
+    });
+    connection.query("USE brewsimdb", function(err, result){
+        if(err) throw err;
+        console.log(result);
+    });
+});*/
+
+
+
+
+
+
+
+
+
+
+
+
+
 var hops_type = {};
 app.get('/', function(req, res) {
+    console.log("entered into main page.");
   connection.query('SELECT * FROM hops;', function (err, result) {
     if (err) throw err
-
+    console.log("Got this for ya!" + result);
     hops_type = {'print' : result};
     res.render('index', hops_type);
   });
