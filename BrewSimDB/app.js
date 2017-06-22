7fde6ab1e9d112d32698d4cf77b45db56900de84
Kristofer Hoadley <kristoferhoadley@gmail.com>
2017-06-22 10:52:34 -0500

    Added getters to retrieve incredients from a recipe.

--- BrewSimDB/app.js
+++ BrewSimDB/app.js
@@ -55,7 +55,8 @@ app.get('/', function(req, res) {
     // for whatever reason (probably async) if I put the connect line directly below the initializeDatabase line it can't connect because the database doesn't exist yet. Putting it here gives the database initialization time to complete first.
     databaseHandler.connect('BrewSimDB', process.env.DB_USER, process.env.DB_PASSWORD);
     console.log("entered into main page.");
-    databaseHandler.getAllGrain(function (result) {
+    // below is query testing line. Will display names of results to index.
+    databaseHandler.getGrainsByRecipeName('Hoppiness is an IPA',function (result) {
         console.log('returned this: ' + result);
         item_type = {'print' : result};
         res.render('index', item_type);
