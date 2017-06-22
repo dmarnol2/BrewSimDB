494e7832a3abd5fc984ab526896e07282b9f7f03
Gabo Martinez <gabmart02@gmail.com>
2017-06-22 13:21:34 -0400

    removed the routes I'd added to the routes/index.js when I was trying to modularize the routes

---
+++ BrewSimDB/routes/hops.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+
+//hops routes.. every route here starts with /hops
+
+
+router.get('/', function(req, res){
+
+});
+
+
+
+module.exports = router;
